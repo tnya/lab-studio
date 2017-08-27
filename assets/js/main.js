e19042b2d4f2ec27838d5752af4b1beaf732bb65
Tanya Ramirez <tcramire@gmail.com>
2017-08-27 04:42:20 -0300

    arreglos finales

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', function () {
+  const distanciaVertical = window.pageYOffset || document.documentElement.scrollTop,
+  header = document.getElementById('header');
+  
+  if (distanciaVertical > 550) {
+    header.classList.add("navbar-color");
+  } else {
+    header.classList.remove("navbar-color");
+  }
+});
